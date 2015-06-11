2f0fa2561808991554e40ca0ede8a97a778cb612
Lucas Possatti <lucas_possatti@hotmail.com>
2015-06-11 01:10:32 -0300

    Add messages to the uninstall script
    Now the uninstall script displays some messages showing what has
    been removed.

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+#
+# This script uninstall pokemonsay.
+#
+
+install_path="$HOME/.config/pokemonsay/"
+bin_path="$HOME/bin/pokemonsay"
+
 # Remove the install directory
-rm -r "$HOME/.config/pokemonsay/"
+rm -r $install_path
 
 # Remove the bin file
-rm "$HOME/bin/pokemonsay"
+rm $bin_path
+
+# Say what's going on.
+echo "'$install_path' directory was removed."
+echo "'$bin_path' file was removed."
