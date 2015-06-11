0ca94d28fb096614e3e90ef0f29d9abd3be30c59
Lucas Possatti <lucas_possatti@hotmail.com>
2015-06-10 23:47:08 -0300

    Create a script to change the pokemon images the way I want it
    The script will mirror the pokemon images so that they will be
    looking to the right. And it will crop the image in order to
    remove all the whitespace around the pokemon.

---
+++ fix_images.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for image in ./scrapped-data/*.png
+do
+	# Mirror the image in the horizontal direction so
+	# that the pokemon will be looking to the right.
+	convert -flop "$image" "$image"
+	# Trim the useless empty space around the pokemon.
+	convert -trim "$image" "$image"
+done
