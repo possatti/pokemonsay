12a87a3ac6af8e795d3b1a1925b716e476146e77
hdirks2 <114555560+hdirks2@users.noreply.github.com>
2023-05-08 00:37:56 -0700

    Cow images https://github.com/msikma/pokesprite They look much better than the ones downloaded from bulbapedia

--- fix_images.sh
+++ fix_images.sh
@@ -5,9 +5,6 @@ do
 	# Mirror the image in the horizontal direction so that the pokemon will be looking to the right.
 	convert -flop "$image" "$image"
 
-	# Resize the image to 50x50 pixels (or close to that if image isn't a perfect square).
-	convert "$image" -resize 50x50 "$image"
-
 	# Trim the useless empty space around the pokemon.
 	convert -trim "$image" "$image"
 done
